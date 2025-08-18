38bee9138d25905ee30e62cff2ddcedc4c92bad7
ezdin <ezedinawol130@gmail.com>
2025-08-18 10:29:02 +0300

    calendar changed from normal calendar to day calendar

--- lib/widgets/calendar_widget.dart
+++ lib/widgets/calendar_widget.dart
@@ -1,37 +1,121 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/intl.dart';
 
 import '../models/task_model.dart';
 
-class CalendarWidget extends StatelessWidget {
+class DaySelector extends StatefulWidget {
   final List<TaskModel> tasks;
-  const CalendarWidget({super.key, required this.tasks});
+  final ValueChanged<DateTime>? onDaySelected;
+
+  const DaySelector({
+    super.key,
+    required this.tasks,
+    this.onDaySelected,
+  });
+
+  @override
+  State<DaySelector> createState() => _DaySelectorState();
+}
+
+class _DaySelectorState extends State<DaySelector> {
+  DateTime _selectedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      firstDay: DateTime.utc(2020, 1, 1),
-      lastDay: DateTime.utc(2100, 12, 31),
-      focusedDay: DateTime.now(),
-      calendarBuilders: CalendarBuilders(
-        markerBuilder: (context, day, events) {
-          final dayTasks = tasks.where((t) => isSameDay(t.deadline, day));
-          if (dayTasks.isNotEmpty) {
-            return Positioned(
-              bottom: 1,
-              child: Container(
-                width: 6,
-                height: 6,
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                  shape: BoxShape.circle,
-                ),
+    final now = DateTime.now();
+
+    final days = List.generate(
+      14,
+          (index) => DateTime(now.year, now.month, now.day + index),
+    );
+
+    return SizedBox(
+      height: 100,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        itemCount: days.length,
+        itemBuilder: (context, index) {
+          final day = days[index];
+          final isSelected = isSameDay(day, _selectedDay);
+
+          final hasTasks = widget.tasks.any((t) => isSameDay(t.deadline, day));
+
+          return GestureDetector(
+              onTap: () {
+                setState(() => _selectedDay = day);
+                if (widget.onDaySelected != null) {
+                  widget.onDaySelected!(day);
+                }
+              },
+
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 250),
+              margin: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+              padding: const EdgeInsets.all(14),
+              width: 70,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: isSelected
+                    ? const LinearGradient(
+                  colors: [Color(0xFF4facfe), Color(0xFF00f2fe)],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                )
+                    : null,
+                color: isSelected ? null : Colors.grey.shade100,
+                boxShadow: isSelected
+                    ? [
+                  BoxShadow(
+                    color: Colors.blue.withOpacity(0.3),
+                    blurRadius: 10,
+                    offset: const Offset(0, 4),
+                  ),
+                ]
+                    : [],
               ),
-            );
-          }
-          return null;
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    DateFormat.E().format(day),
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: isSelected ? Colors.white : Colors.black87,
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  Text(
+                    DateFormat.d().format(day),
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: isSelected ? Colors.white : Colors.black87,
+                    ),
+                  ),
+                  if (hasTasks) ...[
+                    const SizedBox(height: 4),
+                    Container(
+                      width: 6,
+                      height: 6,
+                      decoration: BoxDecoration(
+                        color: isSelected ? Colors.white : Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                    )
+                  ],
+                ],
+              ),
+            ),
+
+          );
         },
       ),
     );
+  }
+
+  bool isSameDay(DateTime a, DateTime b) {
+    return a.year == b.year && a.month == b.month && a.day == b.day;
   }
 }
